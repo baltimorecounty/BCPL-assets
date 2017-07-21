74ec27e45ec475550c51afb926979b5062bcd563
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-07-21 12:26:40 -0400

    Adding in Mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,93 +1,74 @@
-const 
-	babel = require('gulp-babel'),
-	clean = require('gulp-clean'),
-	concat = require('gulp-concat'),
-	cssnano = require('gulp-cssnano'),
-	gulp = require('gulp'),
-	jshint = require('gulp-jshint'),
-	pug = require('gulp-pug'),
-	rename = require('gulp-rename'),
-	runSequence = require('run-sequence'),
-	sass = require('gulp-sass'),
-	stripCode = require('gulp-strip-code'),
-	stylish = require('jshint-stylish'),
-	uglify = require('gulp-uglify'),
-	util = require('gulp-util');
-	
-gulp.task('clean', () => {
-	return gulp.src('dist')
-		.pipe(clean());
-});
+const babel = require('gulp-babel');
+const clean = require('gulp-clean');
+const concat = require('gulp-concat');
+const cssnano = require('gulp-cssnano');
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const pug = require('gulp-pug');
+const rename = require('gulp-rename');
+const runSequence = require('run-sequence');
+const sass = require('gulp-sass');
+const stripCode = require('gulp-strip-code');
+const stylish = require('jshint-stylish');
+const uglify = require('gulp-uglify');
+const util = require('gulp-util');
 
-gulp.task('process-scss', () => {
-	return gulp.src(['stylesheets/master.scss', 'stylesheets/home.scss'])
+gulp.task('clean', () => gulp.src('dist')
+		.pipe(clean()));
+
+gulp.task('process-scss', () => gulp.src(['stylesheets/master.scss', 'stylesheets/home.scss'])
 		.pipe(sass())
 		.pipe(cssnano({ autoprefixer: false }))
 		.pipe(rename({ suffix: '.min' }))
-		.pipe(gulp.dest('dist/css'));
-});
+		.pipe(gulp.dest('dist/css')));
 
-gulp.task('minify-js', ['process-js', 'move-page-specific-js'], () => {
-	return gulp.src(['dist/js/**/*.js'])
+gulp.task('minify-js', ['process-js', 'move-page-specific-js'], () => gulp.src(['dist/js/**/*.js'])
 		.pipe(uglify())
 		.on('error', (err) => { util.log(util.colors.red('[Error]'), err.toString()); })
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('dist/js'));
-});
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest('dist/js')));
 
-gulp.task('process-js', () => {
-	return gulp.src(['js/utility/namespacer.js', 'js/utility/*.js', 'js/**/*.js', '!js/vendor/**/*.js', '!js/page-specific/**/*.js'])
+gulp.task('process-js', () => gulp.src(['js/utility/namespacer.js', 'js/utility/*.js', 'js/**/*.js', '!js/vendor/**/*.js', '!js/page-specific/**/*.js'])
 		.pipe(jshint({
-			esversion: 6
+			esversion: 6,
 		}))
 		.pipe(jshint.reporter(stylish))
 		.pipe(babel({
-			presets: ['es2015']
+			presets: ['es2015'],
 		}))
 		.pipe(stripCode({
 			start_comment: 'test-code',
-			end_comment: 'end-test-code'
+			end_comment: 'end-test-code',
 		}))
 		.pipe(concat('master.js'))
-		.pipe(gulp.dest('dist/js'));
-});
+		.pipe(gulp.dest('dist/js')));
 
-gulp.task('move-page-specific-js', () => {
-	return gulp.src('js/page-specific/**/*.js')
+gulp.task('move-page-specific-js', () => gulp.src('js/page-specific/**/*.js')
 		.pipe(jshint({
-			esversion: 6
+			esversion: 6,
 		}))
 		.pipe(jshint.reporter(stylish))
 		.pipe(babel({
-			presets: ['es2015']
+			presets: ['es2015'],
 		}))
-		.pipe(gulp.dest('dist/js/page-specific'));
-});
+		.pipe(gulp.dest('dist/js/page-specific')));
 
 gulp.task('move-vendor-js', () => {
 	gulp.src('js/vendor/**/*.js')
 		.pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('move-images',  () => {
-	return gulp.src('images/**/*')
-		.pipe(gulp.dest('dist/images'));
-});
+gulp.task('move-images', () => gulp.src('images/**/*')
+		.pipe(gulp.dest('dist/images')));
 
-gulp.task('move-fonts',  () => {
-	return gulp.src('fonts/**.*')
-		.pipe(gulp.dest('dist/fonts'));
-});
+gulp.task('move-fonts', () => gulp.src('fonts/**.*')
+		.pipe(gulp.dest('dist/fonts')));
 
-gulp.task('process-pug', () => {
-	return gulp.src(['mockups/*.pug', 'mockups/support/*.pug'])
+gulp.task('process-pug', () => gulp.src(['mockups/*.pug', 'mockups/support/*.pug'])
 		.pipe(pug())
-		.pipe(gulp.dest('dist'));
-});
+		.pipe(gulp.dest('dist')));
 
-gulp.task('default', ['clean'], (callback) => {
-	return runSequence(['process-pug', 'process-scss', 'minify-js', 'move-vendor-js', 'move-images', 'move-fonts'], callback);
-});
+gulp.task('default', ['clean'], callback => runSequence(['process-pug', 'process-scss', 'minify-js', 'move-vendor-js', 'move-images', 'move-fonts'], callback));
 
 gulp.task('watcher', () => {
 	gulp.watch('**/*.pug', ['default']);
