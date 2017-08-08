5e57c9bd60289f262f8767a5476beeff4397d324
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-08-08 12:59:46 -0400

    Pager finished

--- dist/js/page-specific/search-results.js
+++ dist/js/page-specific/search-results.js
@@ -11,9 +11,37 @@ bcpl.pageSpecific.search = function ($, Handlebars, querystringer) {
 		});
 	};
 
+	var buildArrayFromRange = function buildArrayFromRange(start, end, current) {
+		return Array.from({ length: end - start + 1 }, function (value, key) {
+			return { page: key + start, isCurrent: key + start === current };
+		});
+	};
+
+	var getPageNumbers = function getPageNumbers(current, max) {
+		if (max <= 1 || max < current) return 1;
+
+		var difference = max - current;
+
+		if (current <= 3 && max <= 5) return buildArrayFromRange(1, max, current);
+
+		if (current <= 3 && max >= 5) return buildArrayFromRange(1, 5, current);
+
+		if (difference >= 2) return buildArrayFromRange(current - 2, current + 2, current);
+
+		if (max <= 5) return buildArrayFromRange(1, max, current);
+
+		return buildArrayFromRange(max - 4, max, current);
+	};
+
 	var searchSuccessHandler = function searchSuccessHandler(data) {
+		var templateData = data;
+
+		templateData.ResultsPerPageButtons = [{ count: 10, isCurrent: data.ResultsPerPage === 10 }, { count: 25, isCurrent: data.ResultsPerPage === 25 }, { count: 50, isCurrent: data.ResultsPerPage === 50 }, { count: 100, isCurrent: data.ResultsPerPage === 100 }];
+
+		templateData.PageNumbers = getPageNumbers(templateData.CurrentPage, templateData.Pages);
+
 		getMaterialTypeData(function (materialTypes) {
-			_.map(data, function (record) {
+			_.map(templateData.SearchResults, function (record) {
 				record.icon = materialTypes[_.findIndex(materialTypes, {
 					id: parseInt(record.PrimaryTypeOfMaterial, 10)
 				})].icon;
@@ -21,7 +49,7 @@ bcpl.pageSpecific.search = function ($, Handlebars, querystringer) {
 
 			var source = $('#search-results-template').html();
 			var template = Handlebars.compile(source);
-			var html = template(data);
+			var html = template(templateData);
 			$('#search-results').html(html);
 		});
 	};
@@ -46,14 +74,16 @@ bcpl.pageSpecific.search = function ($, Handlebars, querystringer) {
 
 	var init = function init() {
 		var querystring = querystringer.getAsDictionary();
-		var searchTerm = querystring.q;
-		var pageNumber = querystring.page;
-		var resultsPerPage = querystring.resultsperpage;
+		var searchTerm = querystring.q.trim();
+		var pageNumber = querystring.page ? querystring.page : 1;
+		var resultsPerPage = querystring.resultsperpage ? querystring.resultsperpage : 10;
 
 		$(document).on('click', '#pager .results-per-page button', { searchTerm: searchTerm, pageNumber: pageNumber, resultsPerPage: resultsPerPage }, resultCountButtonClicked);
 		$(document).on('click', '#pager .page-numbers button', { searchTerm: searchTerm, pageNumber: pageNumber, resultsPerPage: resultsPerPage }, pageButtonClicked);
 
-		$.get('http://ba224964:1000/api/polaris/search/' + searchTerm).then(searchSuccessHandler, searchErrorHandler);
+		if (searchTerm) {
+			$.get('http://ba224964:1000/api/polaris/search/' + searchTerm + '/' + pageNumber + '/' + resultsPerPage).then(searchSuccessHandler, searchErrorHandler);
+		}
 	};
 
 	return {
--- js/page-specific/search-results.js
+++ js/page-specific/search-results.js
@@ -5,9 +5,40 @@ bcpl.pageSpecific.search = (($, Handlebars, querystringer) => {
 		$.ajax('http://ba224964:3000/data/primaryMaterialType.json').then(json => processMaterialTypeCallback(json), err => console.log(err));
 	};
 
+	const buildArrayFromRange = (start, end, current) =>
+		Array.from({ length: (end - start) + 1 }, (value, key) =>
+			({ page: (key + start), isCurrent: (key + start) === current }));
+
+	const getPageNumbers = (current, max) => {
+		if (max <= 1 || max < current) return 1;
+
+		const difference = max - current;
+
+		if (current <= 3 && max <= 5) return buildArrayFromRange(1, max, current);
+
+		if (current <= 3 && max >= 5) return buildArrayFromRange(1, 5, current);
+
+		if (difference >= 2) return buildArrayFromRange(current - 2, current + 2, current);
+
+		if (max <= 5) return buildArrayFromRange(1, max, current);
+
+		return buildArrayFromRange(max - 4, max, current);
+	};
+
 	const searchSuccessHandler = (data) => {
+		const templateData = data;
+
+		templateData.ResultsPerPageButtons = [
+			{ count: 10, isCurrent: data.ResultsPerPage === 10 },
+			{ count: 25, isCurrent: data.ResultsPerPage === 25 },
+			{ count: 50, isCurrent: data.ResultsPerPage === 50 },
+			{ count: 100, isCurrent: data.ResultsPerPage === 100 },
+		];
+
+		templateData.PageNumbers = getPageNumbers(templateData.CurrentPage, templateData.Pages);
+
 		getMaterialTypeData((materialTypes) => {
-			_.map(data, (record) => {
+			_.map(templateData.SearchResults, (record) => {
 				record.icon = materialTypes[_.findIndex(materialTypes, {
 					id: parseInt(record.PrimaryTypeOfMaterial, 10),
 				})].icon;
@@ -15,7 +46,7 @@ bcpl.pageSpecific.search = (($, Handlebars, querystringer) => {
 
 			const source = $('#search-results-template').html();
 			const template = Handlebars.compile(source);
-			const html = template(data);
+			const html = template(templateData);
 			$('#search-results').html(html);
 		});
 	};
@@ -40,14 +71,16 @@ bcpl.pageSpecific.search = (($, Handlebars, querystringer) => {
 
 	const init = () => {
 		const querystring = querystringer.getAsDictionary();
-		const searchTerm = querystring.q;
-		const pageNumber = querystring.page;
-		const resultsPerPage = querystring.resultsperpage;
+		const searchTerm = querystring.q.trim();
+		const pageNumber = querystring.page ? querystring.page : 1;
+		const resultsPerPage = querystring.resultsperpage ? querystring.resultsperpage : 10;
 
 		$(document).on('click', '#pager .results-per-page button', { searchTerm, pageNumber, resultsPerPage }, resultCountButtonClicked);
 		$(document).on('click', '#pager .page-numbers button', { searchTerm, pageNumber, resultsPerPage }, pageButtonClicked);
 
-		$.get(`http://ba224964:1000/api/polaris/search/${searchTerm}`).then(searchSuccessHandler, searchErrorHandler);
+		if (searchTerm) {
+			$.get(`http://ba224964:1000/api/polaris/search/${searchTerm}/${pageNumber}/${resultsPerPage}`).then(searchSuccessHandler, searchErrorHandler);
+		}
 	};
 
 	return {
