596341fd3be3e48e07f4beffc31f7395df8f00aa
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-10-31 09:14:23 -0400

    Moved filter to constant

--- js/apps/filter-page/directives/filter.js
+++ js/apps/filter-page/directives/filter.js
@@ -1,16 +1,20 @@
 (() => {
 	'use strict';
 
-	const filterLink = function filterLink($scope, element) {
-		$scope.toggleFilter = (activeFilter) => {
-			const $element = angular.element(element);
-
-			$element.find('label').toggleClass('active', $element.has(':checked'));
-			$scope.filterHandler(activeFilter);
-		};
-	};
-
 	const filterDirective = () => {
+		const template = '' +
+			'<label ng-class="{active: activeFilters.indexOf(filterName) !== -1}">' +
+			'<input type="checkbox" ng-click="toggleFilter(filterName)" ng-checked="activeFilters.indexOf(filterName) !== -1" /> {{filterName}}</label>';
+
+		const filterLink = function filterLink($scope, element) {
+			$scope.toggleFilter = (activeFilter) => {
+				const $element = angular.element(element);
+
+				$element.find('label').toggleClass('active', $element.has(':checked'));
+				$scope.filterHandler(activeFilter);
+			};
+		};
+
 		const directive = {
 			scope: {
 				filterHandler: '=',
@@ -18,7 +22,7 @@
 				activeFilters: '='
 			},
 			restrict: 'E',
-			template: '<label ng-class="{active: activeFilters.indexOf(filterName) !== -1}"><input type="checkbox" ng-click="toggleFilter(filterName)" ng-checked="activeFilters.indexOf(filterName) !== -1" /> {{filterName}}</label>',
+			template: template,
 			link: filterLink
 		};
 
