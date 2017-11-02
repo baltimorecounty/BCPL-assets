4e3190ac588eaf38994f942611e185a44504cbf2
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-11-02 13:02:32 -0400

    Fixed unit tests

--- test/apps/filter-page/filterPageCtrl.spec.js
+++ test/apps/filter-page/filterPageCtrl.spec.js
@@ -14,34 +14,40 @@ describe('filterPageCtrl', () => {
 	describe('setActiveFilters', () => {
 		let controller;
 		let $scope;
+		let mockTagFamily;
 
 		beforeEach(angular.mock.inject(function injectController(_$controller_) {
 			$controller = _$controller_;
 			controller = $controller('FilterPageCtrl', { $scope: $scope });
+			mockTagFamily = {
+				name: 'test',
+				type: 'many',
+				tags: ['test1', 'test2', 'test3', 'test4']
+			};
 		}));
 
 		it('should add a filter to an empty list', () => {
 			controller.activeFilters = [];
-			controller.setActiveFilters('test');
+			controller.setActiveFilters('test', mockTagFamily);
 
 			expect(controller.activeFilters[0]).toBe('test');
 		});
 
 		it('should remove an added filter from an empty list', () => {
 			controller.activeFilters = [];
-			controller.setActiveFilters('test');
-			controller.setActiveFilters('test');
+			controller.setActiveFilters('test', mockTagFamily);
+			controller.setActiveFilters('test', mockTagFamily);
 
 			expect(controller.activeFilters.length).toBe(0);
 		});
 
 		it('should remove an added filter from a populated list', () => {
 			controller.activeFilters = [];
-			controller.setActiveFilters('test1');
-			controller.setActiveFilters('test2');
-			controller.setActiveFilters('test3');
+			controller.setActiveFilters('test1', mockTagFamily);
+			controller.setActiveFilters('test2', mockTagFamily);
+			controller.setActiveFilters('test3', mockTagFamily);
 
-			controller.setActiveFilters('test2');
+			controller.setActiveFilters('test2', mockTagFamily);
 
 			expect(controller.activeFilters).toEqual(['test1', 'test3']);
 		});
