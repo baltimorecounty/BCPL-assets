cdbfde6c188a09dd87990c65cf8803d02bad2110
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-12-12 12:40:46 -0500

    Style tweaks

--- dist/js/apps/filter-page/app.js
+++ dist/js/apps/filter-page/app.js
@@ -16,8 +16,8 @@
 			locations: '/js/apps/filter-page/templates/card-locations.html'
 		},
 		urls: {
-			// databases: 'http://ba224964:3100/api/structured-content/databases',
-			databases: 'http://testservices.bcpl.info/api/structured-content/databases',
+			databases: 'http://ba224964:3100/api/structured-content/databases',
+			// databases: 'http://testservices.bcpl.info/api/structured-content/databases',
 			locations: '/sebin/q/r/branch-amenities.json'
 		},
 		filters: {
