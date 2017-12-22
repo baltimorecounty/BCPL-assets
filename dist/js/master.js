35566fd65f16a89e25fc7363f89962a0e3b55bbe
Michael Snyder <mxsnyder@baltimorecountymd.gov>
2017-12-22 11:26:42 -0500

    Stopped using non-minified files

--- dist/js/master.js
+++ dist/js/master.js
@@ -215,7 +215,7 @@ bcpl.constants = {
 	homepage: {
 		urls: {
 			flipper: '/sebin/y/d/homepage-flipper.json',
-			events: 'http://ba224964:3100/api/evanced/signup/events'
+			events: 'https://testservices.bcpl.info/api/evanced/signup/events'
 		}
 	}
 };
