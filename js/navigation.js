930c04356e0675d90651925bcd3e95dba4d5daf2
Marty Powell <jpowel25@gmail.com>
2018-01-25 11:20:34 -0500

    fixed hover problem where delay caused issue

--- js/navigation.js
+++ js/navigation.js
@@ -223,7 +223,7 @@ bcpl.navigation = (($, keyCodes) => {
 		}
 	};
 
-	$(document).on('mouseover', '.nav-and-search:not(.search-is-active) #responsive-sliding-navigation button', navigationMouseover);
+	$(document).on('mouseover', '.nav-and-search:not(.search-is-active) #responsive-sliding-navigation button, #responsive-sliding-navigation .submenu-wrapper', navigationMouseover);
 	$(document).on('mouseleave', '.nav-and-search:not(.search-is-active) #responsive-sliding-navigation button, #responsive-sliding-navigation .submenu-wrapper', navigationMouseleave);
 	$(document).on('keydown', '#responsive-sliding-navigation button', navigationButtonKeyPressed);
 	$(document).on('keydown', '#responsive-sliding-navigation', navigationKeyPressed);
