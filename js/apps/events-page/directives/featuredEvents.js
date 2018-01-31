0718b18c5b60619ed3f0262643ffcfab9672e78c
Marty Powell <jepowell@baltimorecountymd.gov>
2018-01-31 08:32:04 -0500

    refactored controller logic into a link function due to problems with controller as and isolated scope

--- js/apps/events-page/directives/featuredEvents.js
+++ js/apps/events-page/directives/featuredEvents.js
@@ -1,24 +1,60 @@
 ((app) => {
 	'use strict';
 
-	const featuredEventsDirective = (CONSTANTS) => {
+	const featuredEventsLink = (scope, eventsService) => {
+		const branches = scope.branches && scope.branches.length ? scope.branches : [];
+		const eventTypes = scope.eventTypes && scope.eventTypes.length ? scope.eventTypes : [];
+		const resultsToDisplay = scope.resultsToDisplay || 3;
+		const shouldPrioritzeFeatured = !!scope.prioritizeFeatured;
+
+		const buildRequestPayLoad = (limit, locations, events, prioritizeFeatured) => {
+			let payLoad = {
+				Limit: limit,
+				ShouldPrioritzeFeatured: prioritizeFeatured
+			};
+
+			if (branches.length) {
+				payLoad.Locations = locations;
+			}
+
+			if (eventTypes.length) {
+				payLoad.EventTypes = events;
+			}
+
+			return payLoad;
+		};
+		const handleError = (error) => console.error(error);
+
+		const eventRequestPayload = buildRequestPayLoad(resultsToDisplay, branches, eventTypes, shouldPrioritzeFeatured);
+
+		eventsService.getFeaturedEvents(eventRequestPayload)
+			.then(featuredEventList => {
+				scope.featuredEventList = featuredEventList; // eslint-disable-line
+				if (featuredEventList.length === 0) {
+					scope.resultsToDisplay = 0; // eslint-disable-line
+				}
+			})
+			.catch(handleError);// eslint-disable-line no-console
+	};
+
+
+	const featuredEventsDirective = (CONSTANTS, eventsService) => {
 		const directive = {
 			restrict: 'E',
 			scope: {
+				branches: '=',
 				resultsToDisplay: '=',
-				branch: '=',
-				eventType: '='
+				eventTypes: '=',
+				prioritizeFeatured: '='
 			},
 			templateUrl: CONSTANTS.templateUrls.featuredEventsTemplate,
-			controller: 'FeaturedEventsCtrl',
-			controllerAs: 'featuredEvents',
-			bindToController: true
+			link: (scope) => featuredEventsLink(scope, eventsService)
 		};
 
 		return directive;
 	};
 
-	featuredEventsDirective.$inject = ['CONSTANTS'];
+	featuredEventsDirective.$inject = ['CONSTANTS', 'eventsService'];
 
 	app.directive('featuredEvents', featuredEventsDirective);
 })(angular.module('eventsPageApp'));
