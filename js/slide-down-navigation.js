2fd1b291ff871e4233c2b3712df1e7051812a5a0
Marty Powell <jpowel25@gmail.com>
2018-02-08 11:18:44 -0500

    refactor

--- js/slide-down-navigation.js
+++ js/slide-down-navigation.js
@@ -2,41 +2,39 @@ namespacer('bcpl');
 
 bcpl.slideDownNav = (($) => {
 	const slideDownButtonSelector = '.slide-down-nav-item[data-target]';
+	const downArrowClass = 'fa-angle-down';
+	const upArrowClass = 'fa-angle-up';
+	const activeClass = 'active';
+	const animatationInterval = 500;
+
+	const deactivateSlideDownButton = ($btns) => {
+		$btns
+			.removeClass(activeClass)
+			.find('i')
+				.removeClass(upArrowClass)
+				.addClass(downArrowClass);
+	};
+
 	const onSlideDownButtonSelectorClick = (clickEvent) => {
 		clickEvent.preventDefault();
 		const $slideDownButton = $(clickEvent.currentTarget);
 		const targetElmId = $slideDownButton.data('target');
 		const $targetElm = $(`#${targetElmId}`);
-		const $closestContainer = $targetElm.closest('.container');
 		const isTargetVisbile = $targetElm.is(':visible');
-		const downArrowClass = 'fa-angle-down';
-		const upArrowClass = 'fa-angle-up';
-		const activeClass = 'active';
-		const animatationInterval = 500;
 
 		if (isTargetVisbile) {
 			$targetElm
 				.slideUp(animatationInterval);
 
-			$slideDownButton
-				.removeClass(activeClass)
-				.find('i')
-					.removeClass(upArrowClass)
-					.addClass(downArrowClass);
-
+			deactivateSlideDownButton($slideDownButton);
 		}
 		else {
 			$targetElm.siblings().slideUp(animatationInterval, () => {
 				$targetElm
 					.slideDown(animatationInterval);
-				
-				$slideDownButton
-					.siblings()
-						.removeClass(activeClass)
-						.find('i')
-							.removeClass(upArrowClass)
-							.addClass(downArrowClass);
 
+				deactivateSlideDownButton($slideDownButton.siblings());
+				
 				$slideDownButton
 					.addClass(activeClass)
 						.find('i')
