6c9020c462f4adf27a85976f949032e5e9fe2a13
Marty Powell <jpowel25@gmail.com>
2018-02-12 09:12:49 -0500

    updated to a module so it can be shared

--- js/apps/events-page/dataServices/eventsService.js
+++ js/apps/events-page/dataServices/eventsService.js
@@ -1,5 +1,7 @@
-((app, moment) => {
+((moment) => {
 	'use strict';
+
+	const app = angular.module('dataServices', []);
 
 	const eventsService = (CONSTANTS, $http, $q) => {
 		const isEventOnDate = (eventItem, eventDate) => {
@@ -114,7 +116,7 @@
 		};
 	};
 
-	eventsService.$inject = ['CONSTANTS', '$http', '$q'];
+	eventsService.$inject = ['events.CONSTANTS', '$http', '$q'];
 
-	app.factory('eventsService', eventsService);
-})(angular.module('eventsPageApp'), window.moment);
+	app.factory('dataServices.eventsService', eventsService);
+})(window.moment);
