57c62d6da6ee6726c0f6f2ac85a1271bb171f5ef
Marty Powell <jpowel25@gmail.com>
2018-02-12 09:13:42 -0500

    renamed the config to denote its for the events-page-app only

---
+++ js/apps/events-page/events-page.config.js
@@ -0,0 +1,26 @@
+((app) => {
+	'use strict';
+
+	const config = ($routeProvider, CONSTANTS) => {
+		$routeProvider
+			.when('/', {
+				templateUrl: CONSTANTS.partialUrls.eventListPartial,
+				controller: 'EventsPageCtrl',
+				controllerAs: 'eventsPage'
+			})
+			.when('/:id', {
+				templateUrl: CONSTANTS.partialUrls.eventDetailsPartial,
+				controller: 'EventDetailsCtrl',
+				controllerAs: 'eventDetailsPage'
+			})
+			.when('/register/:id', {
+				templateUrl: CONSTANTS.partialUrls.eventRegistrationPartial,
+				controller: 'EventRegistrationCtrl',
+				controllerAs: 'eventRegistrationPage'
+			});
+	};
+
+	config.$inject = ['$routeProvider', 'events.CONSTANTS'];
+
+	app.config(config);
+})(angular.module('eventsPageApp'));
