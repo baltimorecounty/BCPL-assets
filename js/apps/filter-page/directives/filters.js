615670e82ac662a2a446696c85c4c21778cbf70f
Marty Powell <jpowel25@gmail.com>
2018-02-14 08:23:00 -0500

    if url param exists add property to filter to show it's active

--- js/apps/filter-page/directives/filters.js
+++ js/apps/filter-page/directives/filters.js
@@ -3,8 +3,29 @@
 
 	const filtersDirective = () => {
 		const filterLink = function filterLink($scope) {
+			const findFilterMatch = (tagName, filter) => tagName.toLowerCase() === filter.toLowerCase();
+
 			$scope.$watch('filterData', () => {
 				$scope.filterFamilies = $scope.filterData ? $scope.filterData.map(addFilterId) : $scope.filterFamilies;
+
+				if ($scope.filterFamilies && $scope.filterFamilies.length) {
+					$scope.filterFamilies.forEach((filterFamily) => {
+						const filterFamilyHasTags = filterFamily && Object.hasOwnProperty.call(filterFamily, 'tags') && filterFamily.tags.length;
+						const tags = filterFamilyHasTags ? 
+							filterFamily.tags : 
+							[];
+						let hasMatch = false;
+						
+						$scope.activeFilters.forEach((filter) => {
+							if (!hasMatch) {
+								hasMatch = !!tags
+									.filter((tagName) => findFilterMatch(tagName, filter)).length;
+							}
+						});
+
+						filterFamily.isFilterActive = hasMatch;
+					});
+				}
 			});
 
 			const addFilterId = (filterFamily) => {
@@ -15,7 +36,6 @@
 				if (newFamily) {
 					newFamily.filterId = newFamily.name.replace(/[^\w]/g, '-');
 				}
-
 				return newFamily;
 			};
 		};
