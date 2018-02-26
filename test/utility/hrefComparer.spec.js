69ce98d2b29fe0f7e5f084ff791d2899c52e2a6c
mxsnyder <mxsnyder@baltimorecountymd.gov>
2018-02-26 10:58:14 -0500

    Added

---
+++ test/utility/hrefComparer.spec.js
@@ -0,0 +1,68 @@
+describe('Href comparer utility', () => {
+	describe('removeFilenameAndTrailingSlash', () => {
+		it('should remove the filename and trailing slash from a URL with the default filename', () => {
+			const url = 'https://www.bcpl.info/test/index.html';
+			const expected = 'https://www.bcpl.info/test';
+
+			const actual = bcpl.utility.hrefComparer.removeFilenameAndTrailingSlash(url, bcpl.utility.hrefComparer.hrefEndingTypes.fileName);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should remove the filename and trailing slash from a URL with a non-default filename', () => {
+			const url = 'https://www.bcpl.info/test/abcdefg.html';
+			const expected = 'https://www.bcpl.info/test/abcdefg.html';
+
+			const actual = bcpl.utility.hrefComparer.removeFilenameAndTrailingSlash(url, bcpl.utility.hrefComparer.hrefEndingTypes.fileName);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should remove the trailing slash from a URL with a trailing slash', () => {
+			const url = 'https://www.bcpl.info/test/';
+			const expected = 'https://www.bcpl.info/test';
+
+			const actual = bcpl.utility.hrefComparer.removeFilenameAndTrailingSlash(url, bcpl.utility.hrefComparer.hrefEndingTypes.slash);
+
+			expect(actual).toEqual(expected);
+		});
+
+		it('should ignore a URL without a filename or trailing slash', () => {
+			const url = 'https://www.bcpl.info/test';
+			const expected = 'https://www.bcpl.info/test';
+
+			const actual = bcpl.utility.hrefComparer.removeFilenameAndTrailingSlash(url, bcpl.utility.hrefComparer.hrefEndingTypes.folderName);
+
+			expect(actual).toEqual(expected);
+		});
+	});
+
+	describe('isSamePage', () => {
+		it('should determine two identical URLs that end in filenames are the same', () => {
+			const url1 = 'https://www.bcpl.info/test/index.html';
+			const url2 = 'https://www.bcpl.info/test/index.html';
+
+			const actual = bcpl.utility.hrefComparer.isSamePage(url1, url2);
+
+			expect(actual).toBe(true);
+		});
+
+		it('should determine two identical URLs that end in slashes are the same', () => {
+			const url1 = 'https://www.bcpl.info/test/';
+			const url2 = 'https://www.bcpl.info/test/';
+
+			const actual = bcpl.utility.hrefComparer.isSamePage(url1, url2);
+
+			expect(actual).toBe(true);
+		});
+
+		it('should determine two identical URLs that end in folders are the same', () => {
+			const url1 = 'https://www.bcpl.info/test';
+			const url2 = 'https://www.bcpl.info/test';
+
+			const actual = bcpl.utility.hrefComparer.isSamePage(url1, url2);
+
+			expect(actual).toBe(true);
+		});
+	});
+});
