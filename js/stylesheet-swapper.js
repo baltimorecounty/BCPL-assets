20ebf53db1548e96a48dbb76725851ad7053cbf3
mxsnyder <mxsnyder@baltimorecountymd.gov>
2018-03-09 12:10:46 -0500

    Initial build

--- js/stylesheet-swapper.js
+++ js/stylesheet-swapper.js
@@ -1,25 +1,38 @@
 namespacer('bcpl');
 
-bcpl.stylesheetSwapper = (() => {
+bcpl.stylesheetSwapper = (($) => {
 	const getLinkTagByHref = (href) => {
-		const linkTags = document.querySelectorAll('link');
-		const linkTagArray = Array.prototype.slice.call(linkTags);
-		const loweredHref = href.toLowerCase();
-
-		const matches = linkTagArray.filter((tag) => {
-			return tag.href.toLowerCase() === loweredHref;
-		});
-
-		if (matches.length) {
-			// If multiple links have the same href, just use the last one
-			// because of the cascading nature of css.
-			return matches[matches.length - 1];
+		if (!href || (typeof href === 'string' && !href.trim())) {
+			return null;
 		}
 
-		return null;
+		const loweredHref = href.toLowerCase();
+
+		// If multiple links have the same href, just use the last one
+		// because of the cascading nature of css.
+		const $targetLinkTag = $(`link[href="${loweredHref}"]`).last();
+
+		return $targetLinkTag[0] || null;
+	};
+
+	const swapLinkHrefs = (targetHref, newHref) => {
+		if ((!targetHref || !newHref) || (typeof targetHref !== 'string' || typeof newHref !== 'string')) {
+			return null;
+		}
+
+		const linkTag = getLinkTagByHref(targetHref);
+
+		if (linkTag && linkTag.attributes && linkTag.attributes.href) {
+			linkTag.attributes.href.value = newHref;
+		}
+
+		return linkTag;
 	};
 
 	return {
-		getLinkTagByHref
+		/* test-code */
+		getLinkTagByHref,
+		/* end-test-code */
+		swapLinkHrefs
 	};
-})();
+})(jQuery);
--- test/stylesheet-swapper.spec.js
+++ test/stylesheet-swapper.spec.js
@@ -26,7 +26,79 @@ describe('Stylesheet swapper', () => {
 
 			const actual = bcpl.stylesheetSwapper.getLinkTagByHref('/example/test1.css');
 
-			expect(actual.href).toBe(expected);
+			expect(actual.attributes.href.value).toBe(expected);
+		});
+
+		it('should return null when provided a blank href', () => {
+			const expected = null;
+
+			const actual = bcpl.stylesheetSwapper.getLinkTagByHref('');
+
+			expect(actual).toBe(expected);
+		});
+
+		it('should return null when provided a null href', () => {
+			const expected = null;
+
+			const actual = bcpl.stylesheetSwapper.getLinkTagByHref(null);
+
+			expect(actual).toBe(expected);
+		});
+
+		it('should return null when provided a undefined href', () => {
+			const expected = null;
+
+			const actual = bcpl.stylesheetSwapper.getLinkTagByHref(undefined);
+
+			expect(actual).toBe(expected);
+		});
+
+		it('should return null when provided a whitespace href', () => {
+			const expected = null;
+
+			const actual = bcpl.stylesheetSwapper.getLinkTagByHref('          ');
+
+			expect(actual).toBe(expected);
+		});
+	});
+
+	describe('swapLinkHrefs', () => {
+		beforeEach((done) => {
+			loadFixtures('stylesheet-swapper.fixture.html');
+			done();
+		});
+
+		it('should be a function', () => {
+			const actual = bcpl.stylesheetSwapper.swapLinkHrefs;
+
+			expect(typeof actual).toBe('function');
+		});
+
+		it('should swap links from a found href', () => {
+			const targetHref = '/example/test2.css';
+			const expected = '/example/test-swapped.css';
+
+			const newLinkTag = bcpl.stylesheetSwapper.swapLinkHrefs(targetHref, expected);
+
+			expect(newLinkTag.attributes.href.value).toBe(expected);
+		});
+
+		it('should return null when provided a falsy targetHref', () => {
+			const targetHref = undefined;
+			const expected = null;
+
+			const newLinkTag = bcpl.stylesheetSwapper.swapLinkHrefs(targetHref, expected);
+
+			expect(newLinkTag).toBe(expected);
+		});
+
+		it('should return null when provided a falsy newHref', () => {
+			const targetHref = '/example/test2.css';
+			const expected = null;
+
+			const newLinkTag = bcpl.stylesheetSwapper.swapLinkHrefs(targetHref, expected);
+
+			expect(newLinkTag).toBe(expected);
 		});
 	});
 });
