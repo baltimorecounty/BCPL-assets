04a81f5da46e863d802a3d2e58e581dc2b44fa91
Marty Powell <jpowel25@gmail.com>
2018-03-14 11:14:14 -0400

    initial revision

--- js/page-specific/libanswers-locations-email-buttons.js
+++ js/page-specific/libanswers-locations-email-buttons.js
@@ -1,21 +1,33 @@
-// These scripts are required to be loaded on the page in order to show the
+/*
+    This script is used to add a contact form for each branch, that is displayed in the modal.
+    Note: This script only needs to be include on the location filter page app
+ */
 (function initEmailButtons($) {
 	const libAnswerIds = [6319, 6864, 6865, 6866, 6867, 6868, 6869, 6870, 6871, 6872, 6873, 6874, 6875, 6876, 6877, 6878, 6879, 6777, 6880, 6881];
 
-	libAnswerIds.forEach((id) => {
-		const head = document.getElementsByTagName('head')[0];
-		const script = document.createElement('script');
-		script.src = `https://api2.libanswers.com/1.0/widgets/${id}`;
-		head.appendChild(script);
-	});
-	setTimeout(() => {
-		$('.s-la-widget-activator').toArray().forEach((widgetElem) => {
-			const $btn = $(widgetElem);
-			let newButtonText = $btn.text();
+	const loadScript = (url) => {
+		let script = document.createElement('script');
+		script.type = 'text/javascript';
+		script.src = url;
+		document.getElementsByTagName('head')[0].appendChild(script);
+	};
 
-			newButtonText = 'Contact ' + newButtonText.replace('Email the ', '').replace(' Branch', '');
-
-			$btn.text(newButtonText);
+	const onFilterCardsLoaded = () => {
+		libAnswerIds.forEach((id) => {
+			loadScript(`https://api2.libanswers.com/1.0/widgets/${id}`);
 		});
-	}, 500);
+	};
+
+	const onBranchEmailClick = (clickEvent) => {
+		clickEvent.preventDefault();
+
+		$(clickEvent.currentTarget)
+			.closest('.branch-email-phone-wrapper')
+			.find('.branch-location-email-button')
+			.find('button')
+			.trigger('click');
+	};
+
+	$(document).on('click', '.branch-email', onBranchEmailClick);
+	$(document).on('bc-filter-cards-loaded', onFilterCardsLoaded);
 }(jQuery));
