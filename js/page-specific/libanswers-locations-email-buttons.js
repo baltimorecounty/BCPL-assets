a8a22e5d85a7ff7cfc710888054dcf56be7f4ec2
Marty Powell <jpowel25@gmail.com>
2018-03-15 08:37:43 -0400

    remove scripts/styles that are  being added to page when filters change

--- js/page-specific/libanswers-locations-email-buttons.js
+++ js/page-specific/libanswers-locations-email-buttons.js
@@ -4,11 +4,13 @@
  */
 (function initEmailButtons($) {
 	const libAnswerIds = [6319, 6864, 6865, 6866, 6867, 6868, 6869, 6870, 6871, 6872, 6873, 6874, 6875, 6876, 6877, 6878, 6879, 6777, 6880, 6881];
-
 	const loadScript = (url) => {
+		removeScriptByUrl(url); // Remove script id if it exists
+
 		let script = document.createElement('script');
 		script.type = 'text/javascript';
 		script.src = url;
+
 		document.getElementsByTagName('head')[0].appendChild(script);
 	};
 
@@ -16,6 +18,28 @@
 		libAnswerIds.forEach((id) => {
 			loadScript(`https://api2.libanswers.com/1.0/widgets/${id}`);
 		});
+
+		setTimeout(() => {
+			removeDuplicateScriptsAndStyles();
+		}, 1000);
+	};
+
+	const removeDuplicateScriptsAndStyles = () => {
+		removeScriptByUrl('//api2.libanswers.com/js2.18.5/LibAnswers_widget.min.js', true);
+		removeLibAnswersCss();
+	};
+
+	const removeLibAnswersCss = () => {
+		const targetLibAnswerCssRule = '.s-la-widget .btn-default';
+		$(`style:contains(${targetLibAnswerCssRule})`).remove();
+	};
+
+	const removeScriptByUrl = (url, isDuplicate) => {
+		const selector = isDuplicate ?
+			`script[src*="${url}"]:not(:first)` :
+			`script[src*="${url}"]`;
+
+		$(selector).remove();
 	};
 
 	const onBranchEmailClick = (clickEvent) => {
@@ -28,6 +52,7 @@
 			.trigger('click');
 	};
 
-	$(document).on('click', '.branch-email', onBranchEmailClick);
-	$(document).on('bc-filter-cards-loaded', onFilterCardsLoaded);
+	$(document)
+		.on('click', '.branch-email', onBranchEmailClick)
+		.on('bc-filter-cards-loaded', onFilterCardsLoaded);
 }(jQuery));
