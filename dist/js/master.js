dbbe5a8e0efc49df463ebd227478cb3d75200693
Marty Powell <jpowel25@gmail.com>
2018-03-16 12:47:47 -0400

    rebuild

--- dist/js/master.js
+++ dist/js/master.js
@@ -672,6 +672,124 @@ bcpl.boostrapCollapseHelper = function ($) {
 }(jQuery);
 'use strict';
 
+/**
+ * Requires jQuery and Bootstrap
+ */
+namespacer('bcpl');
+
+bcpl.breadCrumbs = function breadCrumbs($) {
+	var templates = {
+		popover: '<div class="popover breadcrumb-popover" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>'
+	};
+	var classNames = {
+		firstBreadCrumb: 'breadcrumb-first',
+		hiddenBreadCrumbContainer: 'hidden-breadcrumb-container',
+		hiddenBreadCrumbTrigger: 'hidden-breadcrumb-trigger',
+		hiddenBreadCrumbPopover: 'hidden-breadcrumb-popover'
+	};
+
+	var buildBreadCrumbHtml = function buildBreadCrumbHtml(hiddenBreadCrumbTriggerClassName) {
+		var hiddenBreadCrumbTriggerHtml = buildBreadCrumbTrigger(hiddenBreadCrumbTriggerClassName);
+		return '<div class="' + classNames.hiddenBreadCrumbContainer + ' ' + classNames.hiddenBreadCrumbPopover + ' breadcrumb breadcrumb-alt" data-toggle="popover" id="tip1"><div class="tooltip-arrow"></div>' + hiddenBreadCrumbTriggerHtml + '</div>';
+	};
+
+	var buildBreadCrumbList = function buildBreadCrumbList($hiddenBreadCrumbs) {
+		var listItems = $hiddenBreadCrumbs.toArray().map(function (hiddenBreadCrumbElm) {
+			var $clonedBreadCrumb = $(hiddenBreadCrumbElm).clone();
+			$clonedBreadCrumb.removeClass('breadcrumb').removeAttr('style');
+
+			return '<li>' + $clonedBreadCrumb[0].outerHTML + '</li>';
+		}).join('');
+
+		return listItems.length ? '<ul class="hidden-breadcrumb-list">' + listItems + '</ul>' : '';
+	};
+
+	var buildBreadCrumbTrigger = function buildBreadCrumbTrigger(breadCrumbTrigger) {
+		return '<span class="' + breadCrumbTrigger + '"><i class="fa fa-circle" aria-hidden="true"></i><i class="fa fa-circle" aria-hidden="true"></i><i class="fa fa-circle" aria-hidden="true"></i></span>';
+	};
+
+	var cleanBreadCrumbs = function cleanBreadCrumbs() {
+		var breadCrumbContainer = '.breadcrumbs-wrapper';
+		var childSelector = '.breadcrumbs-wrapper a, .breadcrumbs-wrapper span';
+		var $childHtml = $(childSelector);
+
+		$childHtml.html(function (i, html) {
+			return html.replace(/(?:\r\n|\r|\n|&nbsp;)/g, '');
+		});
+
+		$(childSelector).remove();
+
+		$(breadCrumbContainer).append($childHtml);
+	};
+
+	var collapseBreadCrumbs = function collapseBreadCrumbs($breadCrumbs) {
+		$breadCrumbs.hide();
+	};
+
+	var createHiddenBreadCrumbs = function createHiddenBreadCrumbs() {
+		var hiddenBreadCrumbHtml = buildBreadCrumbHtml(classNames.hiddenBreadCrumbTrigger);
+
+		$('.' + classNames.firstBreadCrumb).after(hiddenBreadCrumbHtml);
+	};
+
+	var $getBreadCrumbs = function $getBreadCrumbs() {
+		return $('.breadcrumbs-wrapper').find('.breadcrumb');
+	};
+
+	var $getBreadCrumbsToHide = function $getBreadCrumbsToHide($breadCrumbs) {
+		return $breadCrumbs.not(':first,:last');
+	};
+
+	var initHiddenBreadCrumbsPopover = function initHiddenBreadCrumbsPopover($hiddenBreadCrumbs) {
+		var hiddenBreadCrumbList = buildBreadCrumbList($hiddenBreadCrumbs);
+		$('.' + classNames.hiddenBreadCrumbPopover).popover({
+			content: hiddenBreadCrumbList,
+			html: true,
+			placement: 'bottom',
+			template: templates.popover
+		});
+	};
+	var onHiddenBreadCrumbTriggerClick = function onHiddenBreadCrumbTriggerClick(clickEvent) {
+		$(clickEvent.currentTarget).toggleClass('active');
+	};
+
+	var init = function init(breadcrumbThreshold) {
+		var breadcrumbThresholdLimit = breadcrumbThreshold || 3;
+
+		$(function onDocumentReady() {
+			cleanBreadCrumbs();
+
+			var $breadCrumbs = $getBreadCrumbs();
+			var numberOfBreadCrumbs = $breadCrumbs.length;
+
+			if (numberOfBreadCrumbs > breadcrumbThresholdLimit) {
+				var $hiddenBreadCrumbs = $getBreadCrumbsToHide($breadCrumbs);
+
+				collapseBreadCrumbs($hiddenBreadCrumbs);
+
+				createHiddenBreadCrumbs();
+
+				initHiddenBreadCrumbsPopover($hiddenBreadCrumbs);
+			}
+		});
+	};
+
+	$(document).on('click', '.hidden-breadcrumb-container', onHiddenBreadCrumbTriggerClick);
+
+	return {
+		cleanBreadCrumbs: cleanBreadCrumbs,
+		collapseBreadCrumbs: collapseBreadCrumbs,
+		$getBreadCrumbs: $getBreadCrumbs,
+		$getBreadCrumbsToHide: $getBreadCrumbsToHide,
+		init: init
+	};
+}(jQuery);
+
+$(document).ready(function () {
+	bcpl.breadCrumbs.init();
+});
+'use strict';
+
 namespacer('bcpl');
 
 bcpl.contraster = function ($, browserStorage) {
