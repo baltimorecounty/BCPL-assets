3cd72eff3f6961aeacb97d59b7c865328cb323ff
Marty Powell <jpowel25@gmail.com>
2018-03-20 14:15:01 -0400

    rolled back constants

--- js/apps/events-page/constants.js
+++ js/apps/events-page/constants.js
@@ -4,8 +4,8 @@
 	const app = angular.module('events', []);
 
 	var constants = {
-		// baseUrl: 'https://testservices.bcpl.info',
-		baseUrl: 'http://oit226471:1919',
+		baseUrl: 'https://testservices.bcpl.info',
+		// baseUrl: 'http://oit226471:1919',
 		serviceUrls: {
 			events: '/api/evanced/signup/events',
 			eventRegistration: '/api/evanced/signup/registration',
