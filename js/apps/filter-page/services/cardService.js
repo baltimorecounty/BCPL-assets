c3e7ce121b87bd9499a8933b4214008182acea94
mxsnyder <mxsnyder@baltimorecountymd.gov>
2018-03-21 09:26:10 -0400

    Simplified and made filter type a setting.

--- js/apps/filter-page/services/cardService.js
+++ js/apps/filter-page/services/cardService.js
@@ -2,16 +2,8 @@
 	'use strict';
 
 	const cardService = ($location, $window, $injector) => {
-		const getFileNameWithoutExtension = (path) => {
-			const pathParts = path.split('/');
-			const lastPathPart = pathParts[pathParts.length - 1];
-			const noExtension = lastPathPart.split('.')[0];
-			return noExtension;
-		};
-
-		const get = (afterDataLoadedCallback) => {
-			const filenameWithoutExtension = getFileNameWithoutExtension($window.location.pathname);
-			const dataService = $injector.get(`${filenameWithoutExtension}Service`);
+		const get = (afterDataLoadedCallback, filterType) => {
+			const dataService = $injector.get(`${filterType}Service`);
 
 			dataService.get((data) => {
 				const sortedData = _.sortBy(data, dataItem => dataItem.Title);
