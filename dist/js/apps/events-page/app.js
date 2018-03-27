dc8b60773d90d1bf33c70e9819dec4ee66f5af05
Marty Powell <jpowel25@gmail.com>
2018-03-27 13:44:07 -0400

    rebuild

--- dist/js/apps/events-page/app.js
+++ dist/js/apps/events-page/app.js
@@ -158,8 +158,8 @@ bcpl.boostrapCollapseHelper = function ($) {
 	var app = angular.module('events', []);
 
 	var constants = {
-		baseUrl: 'https://testservices.bcpl.info',
-		// baseUrl: 'http://oit226471:1919',
+		// baseUrl: 'https://testservices.bcpl.info',
+		baseUrl: 'http://oit226471:1919',
 		serviceUrls: {
 			events: '/api/evanced/signup/events',
 			eventRegistration: '/api/evanced/signup/registration',
@@ -610,6 +610,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 		var id = $routeParams.id;
 		var vm = this;
 
+		vm.showGroups = false;
 		vm.isGroup = 'false';
 		vm.isSubmitted = false;
 		vm.isLoadingResults = false;
