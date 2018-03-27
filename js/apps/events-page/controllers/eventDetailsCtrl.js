10d10758384b67df8010bb0fdfc184fe7540f965
Marty Powell <jpowel25@gmail.com>
2018-03-27 11:09:52 -0400

    added download events

--- js/apps/events-page/controllers/eventDetailsCtrl.js
+++ js/apps/events-page/controllers/eventDetailsCtrl.js
@@ -1,9 +1,10 @@
 ((app, ICS) => {
 	'use strict';
 
-	const EventDetailsCtrl = function EventsPageCtrl($scope, $timeout, $routeParams, CONSTANTS, eventsService, dateUtilityService) {
+	const EventDetailsCtrl = function EventsPageCtrl($scope, $window, $timeout, $routeParams, CONSTANTS, eventsService, dateUtilityService, emailUtilityService, downloadCalendarEventService) {
 		const vm = this;
 		const id = $routeParams.id;
+
 
 		vm.data = {};
 		vm.data.EventStartDate = '';
@@ -15,28 +16,18 @@
 
 		const processEventData = (data) => {
 			vm.data = data;
-			vm.data.EventStartDate = moment(vm.data.EventStart).format('MMMM D, YYYY');
+			vm.data.EventStartDate = $window.moment(vm.data.EventStart).format('MMMM D, YYYY');
 			vm.data.EventSchedule = dateUtilityService.formatSchedule(vm.data.EventStart, vm.data.EventLength, vm.data.AllDay);
 			vm.isRegistrationRequired = vm.data.RegistrationTypeCodeEnum !== 0;
-			vm.isOver = moment().isAfter(moment(vm.data.EventStart).add(vm.data.EventLength, 'm'));
+			vm.isOver = $window.moment().isAfter($window.moment(vm.data.EventStart).add(vm.data.EventLength, 'm'));
 			vm.isLoading = false;
+			vm.shareUrl = emailUtilityService.getShareUrl(vm.data, $window.location.href);
 		};
 
 		vm.downloadEvent = function downloadEvent(clickEvent) {
 			clickEvent.preventDefault();
 
-			const { LocationName, Title } = vm.data;
-			const eventTitle = `Baltimore County Library, ${LocationName} Branch - ${Title}`;
-			const eventLocation = `${LocationName} Branch`;
-			const eventDescription = 'This is an all day event';
-			const eventStartDate = null;
-			const eventEndDate = null;
-
-
-			let calEvent = new ICS();
-
-			calEvent.addEvent(eventTitle, eventDescription, eventLocation, eventStartDate, eventEndDate);
-			calEvent.download(Title);
+			downloadCalendarEventService.downloadCalendarEvent(vm.data);
 		};
 
 		const requestError = (errorResponse) => {
@@ -50,7 +41,7 @@
 			.catch(requestError);
 	};
 
-	EventDetailsCtrl.$inject = ['$scope', '$timeout', '$routeParams', 'events.CONSTANTS', 'dataServices.eventsService', 'dateUtilityService'];
+	EventDetailsCtrl.$inject = ['$scope', '$window', '$timeout', '$routeParams', 'events.CONSTANTS', 'dataServices.eventsService', 'dateUtilityService', 'emailUtilityService', 'downloadCalendarEventService'];
 
 	app.controller('EventDetailsCtrl', EventDetailsCtrl);
 })(angular.module('eventsPageApp'), window.ics);
