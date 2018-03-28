5e44d224648ad524e95475577d5a4b0bd79eae9c
Marty Powell <jpowel25@gmail.com>
2018-03-28 13:03:22 -0400

    updated document ready to es6, reorg

--- js/related-topics-widget.js
+++ js/related-topics-widget.js
@@ -2,12 +2,12 @@ namespacer('bcpl');
 
 bcpl.relatedTopicsWidget = (($) => {
 	const widgetSelector = '.related-topics-widget';
-	const secondaryNavSelector = '.secondary-nav nav';
+	const secondaryNavSelector = '.secondary-nav';
 
 	const moveWidget = ($widget) => $(secondaryNavSelector).after($widget);
 	const showWidget = ($widget) => $widget.removeClass('hidden');
-
-	$(function onReady() {
+	// OnDocument Ready
+	$(() => {
 		const $widget = $(widgetSelector);
 
 		if ($widget.length) {
