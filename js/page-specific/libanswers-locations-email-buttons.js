597c56749467a686e4e18a0ce315bdcf3961628f
Marty Powell <jpowel25@gmail.com>
2018-04-02 14:27:56 -0400

    adjusted code to handle options

--- js/page-specific/libanswers-locations-email-buttons.js
+++ js/page-specific/libanswers-locations-email-buttons.js
@@ -2,14 +2,16 @@
     This script is used to add a contact form for each branch, that is displayed in the modal.
     Note: This script only needs to be include on the location filter page app
  */
-namespacer('bcpl.pageSpecific.libAnswers');
+namespacer('bcpl.pageSpecific');
 
-bcpl.pageSpecific.libAnswers.emailButtons = (function initEmailButtons($) {
-	const libAnswerIds = [6319, 6864, 6865, 6866, 6867, 6868, 6869, 6870, 6871, 6872, 6873, 6874, 6875, 6876, 6877, 6878, 6879, 6777, 6880, 6881];
-	const libAnswerWidgetJs = '//api2.libanswers.com/js2.18.5/LibAnswers_widget.min.js';
+bcpl.pageSpecific.libAnswers = (function initEmailButtons($, constants) {
+	const generalContactFormId = constants.libAnswers.generalBranchId;
+	const libAnswerWidgetJs = constants.libAnswers.widgetJs;
 	const libAnswerCssStyleRule = '.s-la-widget .btn-default';
+	let libAnswerIds;
+	let moduleOptions;
 
-	const loadScript = (url) => {
+	const loadScript = (url, callback) => {
 		removeScriptByUrl(url); // Remove script id if it exists
 
 		let script = document.createElement('script');
@@ -17,16 +19,24 @@ bcpl.pageSpecific.libAnswers.emailButtons = (function initEmailButtons($) {
 		script.src = url;
 
 		document.getElementsByTagName('head')[0].appendChild(script);
+
+		if (callback && typeof callback === 'function') {
+			callback();
+		}
 	};
 
-	const onFilterCardsLoaded = () => {
-		libAnswerIds.forEach((id) => {
+	const loadScripts = (ids) => {
+		ids.forEach((id) => {
 			loadScript(`https://api2.libanswers.com/1.0/widgets/${id}`);
 		});
 
 		setTimeout(() => {
 			removeDuplicateScriptsAndStyles();
 		}, 1000);
+	};
+
+	const onFilterCardsLoaded = () => {
+		loadScripts(libAnswerIds);
 	};
 
 	const removeDuplicateScriptsAndStyles = () => {
@@ -55,22 +65,67 @@ bcpl.pageSpecific.libAnswers.emailButtons = (function initEmailButtons($) {
 		});
 	};
 
+	const setupContactDiv = (targetSelector, id) => {
+		const targetDivHtml = `<div id="s-la-widget-${id}"></div>`;
+		const $libAnswerDiv = $(`#s-la-widget-${id}`);
+		const $targetDiv = $(targetDivHtml).css('display', 'none');
+
+		if (!$libAnswerDiv.length) {
+			$(targetSelector).after($targetDiv);
+		}
+	};
+
+	const setContactButtonMarkup = (id) => {
+		setupContactDiv(moduleOptions.targetSelector, id);
+	};
+
 	const onBranchEmailClick = (clickEvent) => {
 		clickEvent.preventDefault();
 
 		$(clickEvent.currentTarget)
-			.closest('.branch-email-phone-wrapper')
-			.find('.branch-location-email-button')
-			.find('button')
+			.parent()
+			.find('[id*="s-la-widget"]')
 			.trigger('click');
 	};
 
-	$(document)
-		.on('click', '.branch-email', onBranchEmailClick)
-		.on('bc-filter-cards-loaded', onFilterCardsLoaded);
+	const bindEvents = (targetSelector, loadEvent) => {
+		$(document)
+			.on('click', targetSelector, onBranchEmailClick);
+
+		if (loadEvent) {
+			$(document)
+				.on(loadEvent, onFilterCardsLoaded);
+		}
+	};
+
+	const getOptions = (options) => {
+		let newOptions = options || {};
+
+		newOptions = options || {};
+		newOptions.ids = newOptions.ids || [generalContactFormId];
+		newOptions.loadEvent = newOptions.loadEvent || null;
+		newOptions.targetSelector = newOptions.targetSelector || '.branch-email';
+
+		return newOptions;
+	};
+
+	const init = (options) => {
+		moduleOptions = getOptions(options);
+
+		loadScript(libAnswerWidgetJs, () => {
+			moduleOptions.ids.forEach(setContactButtonMarkup);
+
+			if (!moduleOptions.loadEvent) {
+				loadScripts(moduleOptions.ids);
+			}
+
+			bindEvents(moduleOptions.targetSelector, moduleOptions.loadEvent);
+		}); // Load the required javascript if it doesn't exist
+	};
 
 	return {
+		init,
 		removeScriptByUrl,
 		removeStyleTagByContainingRule
 	};
-}(jQuery));
+}(jQuery, bcpl.constants));
