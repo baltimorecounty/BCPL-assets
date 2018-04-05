4cfecff85fe6fd761db92d2245d2e466007fac68
Marty Powell <jpowel25@gmail.com>
2018-04-05 08:57:03 -0400

    updated constants to include two filter templates

--- js/apps/filter-page/constants.js
+++ js/apps/filter-page/constants.js
@@ -4,7 +4,9 @@
 	var constants = {
 		templates: {
 			databases: '/_js/apps/filter-page/templates/card-databases.html',
-			locations: '/_js/apps/filter-page/templates/card-locations.html'
+			locations: '/_js/apps/filter-page/templates/card-locations.html',
+			filter: '/_js/apps/filter-page/templates/filter.html',
+			filters: '/_js/apps/filter-page/templates/filters.html'
 		},
 		urls: {
 			databases: 'https://services.bcpl.info/api/structured-content/databases',
