b3f36b04f314c2c50bf2323560bb05f8e946352f
mxsnyder <mxsnyder@baltimorecountymd.gov>
2018-04-16 08:46:24 -0400

    moved in from polaris repo

---
+++ js/tablenator.js
@@ -0,0 +1,68 @@
+namespacer('bcpl');
+
+bcpl.tablenator = (($, _) => {
+	let $originalTableCache;
+	let breakpoint;
+
+	const buildTwoColumnTables = ($headings, $dataRows) => {
+		const newTableBodyCollection = [];
+
+		$dataRows.each((rowIndex, rowElement) => {
+			const $newTable = $('<table class="tablenator-responsive-table"><tbody></tbody><table>');
+			const $newTableBody = $newTable.find('tbody');
+			const $row = $(rowElement);
+
+			$headings.each((headingIndex, headingElement) => {
+				const headingText = $headings.eq(headingIndex).text();
+				const dataHtml = $row.find('td').eq(headingIndex).html();
+
+				$newTableBody.append(`<tr><td>${headingText}</td><td>${dataHtml}</td></tr>`);
+			});
+
+			newTableBodyCollection.push($newTable);
+		});
+
+		return newTableBodyCollection;
+	};
+
+	const reformat = (tableIndex, tableElement) => {
+		const $table = $(tableElement);
+		const $headings = $table.find('th');
+		const $dataRows = $table.find('tr').not($headings.closest('tr'));
+
+		const newTableBodyCollection = buildTwoColumnTables($headings, $dataRows);
+
+		newTableBodyCollection.map(($newTable) =>
+			$originalTableCache
+				.eq(tableIndex)
+				.parent()
+				.append($newTable));
+
+		$table.detach();
+	};
+
+	const windowResizehandler = (resizeEvent) => {
+		const windowWidth = $(resizeEvent.target).width();
+
+		if (windowWidth < breakpoint) {
+			$originalTableCache.each(reformat);
+		} else {
+			const parentCollection = $('.tablenator-responsive-table').map((index, tableElement) => $(tableElement).parent().get());
+			$.unique(parentCollection).each((index, parentElement) => $(parentElement).empty().append($originalTableCache.eq(index)));
+		}
+	};
+
+	const init = (tableSelector, screenBreakpoint) => {
+		$originalTableCache = $(tableSelector);
+		breakpoint = screenBreakpoint;
+
+		if ($originalTableCache.length) {
+			const lazyWindowResizeHandler = _.debounce(windowResizehandler, 100);
+			$(window).on('resize', lazyWindowResizeHandler);
+		}
+	};
+
+	return {
+		init
+	};
+})(jQuery, _);
