8806b74cae5520aa310974479b45485e54780f4e
Marty Powell <jpowel25@gmail.com>
2018-04-19 15:09:41 -0400

    rollback

--- js/apps/events-page/controllers/eventsPageCtrl.js
+++ js/apps/events-page/controllers/eventsPageCtrl.js
@@ -14,10 +14,10 @@
 		metaService,
 		RequestModel
 	) {
-        window.onload = () => {
-            $window.scrollTo(0, 0); // Ensure the event details are visible on mobile
-        };
-        
+		setTimeout(() => {
+			$window.scrollTo(0, 0); // Ensure the event details are visible on mobile
+		}, 500);
+
 		const vm = this;
 		const filterTypes = ['locations', 'eventTypes', 'ageGroups'];
 
