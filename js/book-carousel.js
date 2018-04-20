98750bb358fb6d264441c329a482471a3d83d019
mxsnyder <mxsnyder@baltimorecountymd.gov>
2018-04-20 14:49:24 -0400

    added slidesToScroll

--- js/book-carousel.js
+++ js/book-carousel.js
@@ -9,6 +9,7 @@ bcpl.bookCarousel = (($, constants) => {
 		prevArrow: '<a href="#"><i class="fa fa-chevron-left" aria-hidden="true"><span>Scroll left</span></i></a>',
 		nextArrow: '<a href="#"><i class="fa fa-chevron-right" aria-hidden="true"><span>Scroll right</span></i></a>',
 		slidesToShow: 3,
+		slidesToScroll: 3,
 		responsive: [{
 			breakpoint: constants.breakpoints.large,
 			settings: {
@@ -111,6 +112,7 @@ bcpl.bookCarousel = (($, constants) => {
 			$.when.apply($, promises).then(() => {
 				if (settings && settings.maxSlides > 0) {
 					slickSettings.slidesToShow = settings.maxSlides;
+					slickSettings.slidesToScroll = settings.maxSlides;
 				}
 
 				$carousels.slick(slickSettings);
