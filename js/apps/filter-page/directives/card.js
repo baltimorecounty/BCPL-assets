4f770d6aedbc20309d9c69153f1683d6e31d6709
Marty Powell <jpowel25@gmail.com>
2018-06-12 10:07:32 -0400

    added handler for google analytics

--- js/apps/filter-page/directives/card.js
+++ js/apps/filter-page/directives/card.js
@@ -1,12 +1,33 @@
-((app) => {
+((app, googleAnalytics) => {
 	'use strict';
+
+	const { trackEvent } = googleAnalytics;
 
 	const cardDirective = ($compile, $injector, $templateRequest, CONSTANTS) => {
 		const cardLink = ($scope, element, attrs) => {
 			$templateRequest(CONSTANTS.templates[attrs.template]).then((html) => {
 				element.append($compile(html)($scope));
 			});
+
+			$scope.track = (action, label, trackingEvent) => {
+				trackingEvent.preventDefault();
+
+				trackEvent({
+					action,
+					category: CONSTANTS.analytics.bcplLocationsCategory,
+					label
+				});
+
+				const target = trackingEvent.currentTarget;
+
+				if (target && target.href) {
+					window.location = target.href;
+				}
+			};
+
 		};
+
+
 
 		const directive = {
 			restrict: 'E',
@@ -26,4 +47,4 @@
 	cardDirective.$inject = ['$compile', '$injector', '$templateRequest', 'CONSTANTS'];
 
 	app.directive('card', cardDirective);
-})(angular.module('filterPageApp'));
+})(angular.module('filterPageApp'), bcpl.utility.googleAnalytics);
