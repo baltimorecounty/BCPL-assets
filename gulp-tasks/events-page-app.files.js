67150893f89baf1d59de5e1137e20196524e53df
Marty Powell <jpowel25@gmail.com>
2018-08-23 09:07:08 -0400

    added factories folder to the build

--- gulp-tasks/events-page-app.files.js
+++ gulp-tasks/events-page-app.files.js
@@ -1,16 +1,17 @@
-const files = (folder) => [
-	'js/apps/shared/**/*.js',
-	'js/bootstrap*.js',
-	`js/apps/${folder}/app.js`,
-	`js/apps/${folder}/constants.js`,
-	`js/apps/${folder}/events-page.config.js`,
-	`js/apps/${folder}/filters/**/*.js`,
-	`js/apps/${folder}/dataServices/**/*.js`,
-	`js/apps/${folder}/services/**/*.js`,
-	`js/apps/${folder}/models/**/*.js`,
-	`js/apps/${folder}/controllers/**/*.js`,
-	`js/apps/${folder}/directives/**/*.js`,
-	`!js/apps/${folder}/directives/**/featuredEvents.js`
+const files = folder => [
+    'js/apps/shared/**/*.js',
+    'js/bootstrap*.js',
+    `js/apps/${folder}/app.js`,
+    `js/apps/${folder}/constants.js`,
+    `js/apps/${folder}/events-page.config.js`,
+    `js/apps/${folder}/factories/**/*.js`,
+    `js/apps/${folder}/filters/**/*.js`,
+    `js/apps/${folder}/dataServices/**/*.js`,
+    `js/apps/${folder}/services/**/*.js`,
+    `js/apps/${folder}/models/**/*.js`,
+    `js/apps/${folder}/controllers/**/*.js`,
+    `js/apps/${folder}/directives/**/*.js`,
+    `!js/apps/${folder}/directives/**/featuredEvents.js`
 ];
 
 module.exports = files;
