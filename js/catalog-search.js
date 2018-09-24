23b20affef3cd474818f0ce0d6c8f49b40847d61
Marty Powell <jpowel25@gmail.com>
2018-09-24 11:40:27 -0400

    adjusted code for google custom search

--- js/catalog-search.js
+++ js/catalog-search.js
@@ -1,7 +1,10 @@
 namespacer('bcpl');
 
-bcpl.catalogSearch = (($, queryStringer, constants) => {
+bcpl.catalogSearch = (($, queryStringer, waitForExistence, constants) => {
+	let intervalChecker;
 	const catalogSearchSelector = '#catalog-search, .catalog-search';
+	const resultsInfoContainerSelector = '.gsc-result-info';
+	const searchCatalogButton = '<td><button id="catalog-search" class="btn btn-primary pull-right">Search the Catalog</button></td>';
 
 	const getCatalogUrl = (searchTerm) => `${constants.baseCatalogUrl}${constants.search.urls.catalog}${searchTerm}`;
 
@@ -9,14 +12,24 @@ bcpl.catalogSearch = (($, queryStringer, constants) => {
 		clickEvent.preventDefault();
 
 		const queryParams = queryStringer.getAsDictionary();
-		const searchTerm = queryParams.term;
+		const searchTerm = queryParams.search;
 
 		window.location = getCatalogUrl(searchTerm);
 	};
 
+	const init = () => {
+		waitForExistence(resultsInfoContainerSelector, () => {
+			$(resultsInfoContainerSelector).closest('td').after(searchCatalogButton);
+		});
+	}
+
 	$(document).on('click', catalogSearchSelector, onCatalogSearchClick);
+
+	$(document).ready(() => {
+		init();
+	});
 
 	return {
 		getCatalogUrl
 	};
-})(jQuery, bcpl.utility.querystringer, bcpl.constants);
+})(jQuery, bcpl.utility.querystringer, bcpl.utility.waitForExistence, bcpl.constants);
