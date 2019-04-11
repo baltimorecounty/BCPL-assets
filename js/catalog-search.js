8c5ed529ce5cb52817a1c5f10c0fe9d2b579f1f9
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-04-11 10:16:34 -0400

    create constant varible and fix issue with calling function

--- js/catalog-search.js
+++ js/catalog-search.js
@@ -4,7 +4,7 @@ bcpl.catalogSearch = (($, queryStringer, waitForExistence, constants) => {
 	const catalogSearchSelector = '#catalog-search, .catalog-search';
 	const resultsInfoContainerSelector = '.gsc-above-wrapper-area-container';
 	const searchCatalogButton = '<td><button id="catalog-search" class="btn btn-primary pull-right">Search the Catalog</button></td>';
-
+	const showResults = "#resInfo-0:contains('About')";
 	const getCatalogUrl = (searchTerm) => `${constants.baseCatalogUrl}${constants.search.urls.catalog}${searchTerm}`;
 
 	const getSearchTerm = () =>{
@@ -19,7 +19,7 @@ bcpl.catalogSearch = (($, queryStringer, waitForExistence, constants) => {
 
 	const init = () => {
 		waitForExistence(resultsInfoContainerSelector, () => {
-			$("#resInfo-0:contains('About')").prepend(`Showing results for ${getSearchTerm} <BR/>`);
+			$(showResults).prepend(`Showing results for ${getSearchTerm()} <BR/>`);
 			$(resultsInfoContainerSelector).find('td').first().after(searchCatalogButton);
 		});
 	};
