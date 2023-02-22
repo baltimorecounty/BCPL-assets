b3fdc5392a50d453b34a754f10475a55b9a97256
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2023-02-22 14:34:42 -0500

    updates to property utility

--- js/utility/addalertproperties.js
+++ js/utility/addalertproperties.js
@@ -1,7 +1,12 @@
 function addPropertyToDiv() {
   const div = document.getElementsByClassName("message-panel");
+  const element = document.getElementsByClassName("form-validation-error");
 
   for (let i = 0; i < div.length; i++) {
+    div[i].setAttribute("role", "alert");
+  }
+
+  for (let i = 0; i < element.length; i++) {
     div[i].setAttribute("role", "alert");
   }
 }
